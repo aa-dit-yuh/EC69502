9dbf624521c1ad90aa4006ef68a81151ab454c29
Aditya Narayan <narayanaditya95@gmail.com>
2017-01-24 13:38:15 +0530

    Refactor. Merge `histogramEqualize` and `histogramMatch` functions.

--- 2/2.cpp
+++ 2/2.cpp
@@ -1,7 +1,6 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
-#include <opencv2/plot.hpp>
 
 #include <cstdint>
 #include <iostream>
@@ -9,7 +8,6 @@
 #include <valarray>
 #include <vector>
 #include <string>
-
 
 std::valarray<int> getHistogram(cv::Mat src)
 {
@@ -19,15 +17,30 @@ std::valarray<int> getHistogram(cv::Mat src)
     return ret;
 }
 
-void equalizeHistogram(cv::Mat& src, cv::Mat& dest)
+std::valarray<int> getCumulativeHistogramNormalized(cv::Mat src)
+{
+    std::valarray<int> ret;
+    if (src.channels() == 3) {
+        cv::cvtColor(src, src, cv::COLOR_BGR2HSV);
+        std::vector<cv::Mat> srcHSV;
+        cv::split(src, srcHSV);
+        ret = getHistogram(srcHSV[2]);
+    }
+    else
+        ret = getHistogram(src);
+    std::partial_sum(begin(ret), end(ret), begin(ret));
+    ret = 255 * ret / ret.max();
+    return ret;
+}
+
+void matchHistogram(cv::Mat& src, cv::Mat& dest, std::valarray<int>& refH_x)
 {
     auto size = src.size();
-    std::valarray<int> h_x = getHistogram(src), H_x(256);
-    std::partial_sum(begin(h_x), end(h_x), begin(H_x));                     // Calculate H(x) = integ{h(x)}
-    H_x = 255 * H_x / H_x.max();                                            // Normalize H(x)
+    std::valarray<int> H_x = getCumulativeHistogramNormalized(src);
     for (auto i = 0; i != size.height; ++i)
-        for (auto j = 0; j != size.width; ++j)
-            dest.at<uint8_t>(i, j) = H_x[src.at<uint8_t>(i, j)];     // equalized(x,y) = H(image(x,y))
+        for (auto j = 0; j != size.width; ++j) {
+            dest.at<uint8_t>(i, j) = *std::upper_bound(begin(refH_x), end(refH_x), H_x[src.at<uint8_t>(i, j)]);     // equalized(x,y) = H(image(x,y))
+    }
 }
 
 int main()
@@ -38,33 +51,30 @@ int main()
               << std::endl;
     int choice; std::cin >> choice;
 
-    switch (choice) {
-    case 1: {
-        std::cout << "Enter the filename of the image: ";
-        std::string file; std::cin >> file;
+    std::cout << "Enter the filename of the input image: ";
+    std::string inputFile; std::cin >> inputFile;
+    cv::Mat input = cv::imread(inputFile, cv::IMREAD_UNCHANGED);
+    if (!input.data) {
+        std::cout << "Invalid input file\n" << std::endl;
+        return main();
+    }
+    else
+        cv::imshow("Original Image", input);
 
-        cv::Mat image = cv::imread(file, cv::IMREAD_UNCHANGED);
-        if (!image.data) {
-            std::cout << "Invalid file\n" << std::endl;
+    std::valarray<int> refH_x(256);
+    switch (choice) {
+    case 1:
+        std::iota(begin(refH_x), end(refH_x), 0);
+        break;
+    case 2: {
+        std::cout << "Enter the filename of reference image: ";
+        std::string refFile; std::cin >> refFile;
+        cv::Mat ref = cv::imread(refFile, cv::IMREAD_UNCHANGED);
+        if (!ref.data) {
+            std::cout << "Invalid reference file\n" << std::endl;
             return main();
         }
-        cv::imshow("Original Image", image);
-
-        if (image.channels() == 3) {
-            cv::cvtColor(image, image, cv::COLOR_BGR2HSV);
-            std::vector<cv::Mat> HSV;
-            cv::split(image, HSV);
-            equalizeHistogram(HSV[0], HSV[0]);
-            cv::cvtColor(image, image, cv::COLOR_HSV2BGR);
-            cv::imshow("Histogram Equalized Image", image);
-            cv::waitKey(0);
-        }
-        else {
-            cv::Mat equalized(image);
-            equalizeHistogram(image, equalized);
-            cv::imshow("Histogram Equalized", equalized);
-            cv::waitKey(0);
-        }
+        refH_x = getCumulativeHistogramNormalized(ref);
         break;
     }
     default:
@@ -72,5 +82,20 @@ int main()
         return main();
         break;
     }
+
+    if (input.channels() == 3) {
+        cv::cvtColor(input, input, cv::COLOR_BGR2HSV);
+        std::vector<cv::Mat> inputHSV;
+        cv::split(input, inputHSV);
+        matchHistogram(inputHSV[2], inputHSV[2], refH_x);
+        cv::merge(inputHSV, input);
+        cv::cvtColor(input, input, cv::COLOR_HSV2BGR);
+    }
+    else
+        matchHistogram(input, input, refH_x);
+    cv::imshow("Histogram Equalized/Matched Image", input);
+    cv::imwrite("matched-" + inputFile, input);
+    cv::waitKey(0);
+
     return 0;
 }
